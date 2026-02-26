5fd8ea97bc0a77ab94d7daae92f9271d77514e65
Vladimir <yarvoldem@yandex.ru>
2026-02-26 16:21:26 +0300

    Сборка проекта. Подготовка к выкладке

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 
+// ВАЖНО для GitHub Pages:
+// - base должен совпадать с именем репозитория (`/weblarek/`),
+//   чтобы пути к собранным скриптам/стилям были корректными на https://<user>.github.io/weblarek/
+// - outDir = 'docs', чтобы можно было публиковать GitHub Pages из папки /docs ветки main
 export default defineConfig({
+  base: '/weblarek/',
+  build: {
+    outDir: 'docs',
+  },
   css: {
     preprocessorOptions: {
       scss: {
